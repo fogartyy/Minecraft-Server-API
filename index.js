74cb37bee21ce5cbc5c792473c5cdbd80cabf70a
fogartyy <kmaster1098765@gmail.com>
2023-03-21 19:25:01 +1300

    name

--- index.js
+++ index.js
@@ -45,12 +45,48 @@ var data = getData();
 
 //set Access-Control-Allow-Origin
 app.use(cors());
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
 
 
 //get data
-app.get('/data', (req, res) => {
+app.get('/data',cors(corsOptions), async (req, res) => {
   //get data
-    var data = getData();
+  const fs = require('fs');
+  //get files from dirrectory sucking.world/playerdata
+  const files = fs.readdirSync('../Sucking/world/stats/');
+  //create array
+  
+  var data = [];
+  //loop through files save data to array with each entry being labeled with the file name as UUID
+  files.forEach(file => async () => {
+    //get data from file
+    var filedata = fs.readFileSync('../Sucking/world/stats/' + file, 'utf8');
+    //parse data from file
+    var parsed = JSON.parse(filedata);
+    //get uuid from file name
+    var uuid = file.replace('.json', '');
+
+
+    const fetchOptions = {
+        method: 'GET'
+    }
+    const response = await fetch(`https://api.mojang.com/user/profile/${uuid}`, fetchOptions);
+    const jsonResponse = await response.json();
+
+
+    //add data to array
+    var array =[];
+    array.push({
+      uuid: uuid,
+      data: parsed,
+        name: jsonResponse.name
+    });
+
+    data.push(array);
+    
+  });
     //send data
     //format text as json
     res.setHeader('Content-Type', 'application/json');
@@ -59,6 +95,7 @@ app.get('/data', (req, res) => {
 
 
 })
+
 
 //get data
 app.get('/', (req, res) => {
