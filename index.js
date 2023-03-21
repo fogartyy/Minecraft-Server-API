e44f98f8f1463fab1f01a2b64c39d711534c9e76
fogartyy <kmaster1098765@gmail.com>
2023-03-21 19:28:29 +1300

    d

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
-//set Access-Control-Allow-Origin
-const cors = require('cors');
-
-
+//set
 const app = express()
 const port = 3000
 
@@ -38,55 +35,13 @@ function getData() {
   
 }
 
-//function that gets data from api based on uuid
-
 //get data
 var data = getData();
 
-//set Access-Control-Allow-Origin
-app.use(cors());
-const corsOptions = {
-    origin: "http://localhost:3000"
-};
-
-
 //get data
-app.get('/data',cors(corsOptions), async (req, res) => {
+app.get('/data', (req, res) => {
   //get data
-  const fs = require('fs');
-  //get files from dirrectory sucking.world/playerdata
-  const files = fs.readdirSync('../Sucking/world/stats/');
-  //create array
-  
-  var data = [];
-  //loop through files save data to array with each entry being labeled with the file name as UUID
-  files.forEach(file => async () => {
-    //get data from file
-    var filedata = fs.readFileSync('../Sucking/world/stats/' + file, 'utf8');
-    //parse data from file
-    var parsed = JSON.parse(filedata);
-    //get uuid from file name
-    var uuid = file.replace('.json', '');
-
-
-    const fetchOptions = {
-        method: 'GET'
-    }
-    const response = await fetch(`https://api.mojang.com/user/profile/${uuid}`, fetchOptions);
-    const jsonResponse = await response.json();
-
-
-    //add data to array
-    var array =[];
-    array.push({
-      uuid: uuid,
-      data: parsed,
-        name: jsonResponse.name
-    });
-
-    data.push(array);
-    
-  });
+    var data = getData();
     //send data
     //format text as json
     res.setHeader('Content-Type', 'application/json');
@@ -95,7 +50,6 @@ app.get('/data',cors(corsOptions), async (req, res) => {
 
 
 })
-
 
 //get data
 app.get('/', (req, res) => {
