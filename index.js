d4070d7321a60bcb71cca253d28a81768e062982
fogartyy <kmaster1098765@gmail.com>
2023-03-22 11:24:18 +1300

    change

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = 3000
 
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var privateKey  = fs.readFileSync('../sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('../sslcert/server.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 
